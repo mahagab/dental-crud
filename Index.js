8669dcddb6085250298bbfe053f69cb73034be7e
Gabriel Mahmoud <79914923+mahagab@users.noreply.github.com>
2024-06-03 13:24:32 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -18,7 +18,8 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  charset: 'utf8mb4'
 });
 
 db.connect(err => {
